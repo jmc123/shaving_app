b99e0bd48a66e2d25803a754bea0a967bcfebff0
James <jamesmcmanus23@gmail.com>
2013-04-02 21:07:16 +0100

    Add devise functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ TestApp::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
+  config.action_mailer.default_url_options = {host: 'http://tranquil-retreat-6368.herokuapp.com/' }
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TestApp::Application.routes.draw do
+  devise_for :users
+
   resources :line_items
 
 
